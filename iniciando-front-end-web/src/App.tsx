f9c7c4850ab442292ba4b7ec1738bbb0f20abb98
Alisson Fernandes de Paula <pfalisson@gmail.com>
2021-02-09 22:14:20 -0300

    Nivel03 - Frontend: API de contexto

--- iniciando-front-end-web/src/App.tsx
+++ iniciando-front-end-web/src/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import AuthContext from './context/AuthContext';
+
 import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
 
-const App: React.FC = () => (
-  <>
-    <SignIn />
-    <GlobalStyle />
-  </>
-);
+const App: React.FC = () => {
+  return (
+    <>
+      <AuthContext.Provider value={{ name: 'Teste' }}>
+        <SignIn />
+      </AuthContext.Provider>
+
+      <GlobalStyle />
+    </>
+  );
+};
 
 export default App;
